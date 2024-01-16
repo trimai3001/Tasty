e6411b9d8deda32c5a997f43fbc6597f017307d9
tri_mai <dotavsdday@gmail.com>
2024-01-16 14:13:39 +0000

    Add icon and menu for navigator (drawer)

--- lib/home.dart
+++ lib/home.dart
@@ -42,40 +42,184 @@ class _MyHomePageState extends State<MyHomePage> {
         child: _widgetOptions[_selectedIndex],
       ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
+            const SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Mai Minh Tri",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text("Chi nhanh trung tam", textAlign: TextAlign.left)
+                    ],
+                  )),
+            ),
             ListTile(
-              title: const Text('Home'),
+              leading: const Icon(Icons.bar_chart_outlined),
+              title: const Text('Tổng quan'),
               selected: _selectedIndex == 0,
               onTap: () {
-                // Update the state of the app
                 _onItemTapped(0);
-                // Then close the drawer
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: const Text('Business'),
+              leading: const Icon(Icons.trolley),
+              title: const Text('Hàng hoá'),
               selected: _selectedIndex == 1,
               onTap: () {
-                // Update the state of the app
                 _onItemTapped(1);
-                // Then close the drawer
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: const Text('School'),
+              leading: const Icon(Icons.warehouse),
+              title: const Text('Kiểm kho'),
               selected: _selectedIndex == 2,
               onTap: () {
-                // Update the state of the app
                 _onItemTapped(2);
-                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.receipt),
+              title: const Text('Hoá đơn'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                _onItemTapped(3);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.clean_hands),
+              title: const Text('Trả hàng'),
+              selected: _selectedIndex == 4,
+              onTap: () {
+                _onItemTapped(4);
+                Navigator.pop(context);
+              },
+            ),
+            // Add more ListTile widgets for additional options...
+            // Example:
+            ListTile(
+              leading: const Icon(Icons.fire_truck),
+              title: const Text('Nhập hàng'),
+              selected: _selectedIndex == 5,
+              onTap: () {
+                _onItemTapped(5);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Trả hàng nhập'),
+              selected: _selectedIndex == 6,
+              onTap: () {
+                _onItemTapped(6);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Chuyển hàng'),
+              selected: _selectedIndex == 7,
+              onTap: () {
+                _onItemTapped(7);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Xuất huỷ'),
+              selected: _selectedIndex == 8,
+              onTap: () {
+                _onItemTapped(8);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Khách hàng'),
+              selected: _selectedIndex == 9,
+              onTap: () {
+                _onItemTapped(9);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Nhà cung cấp'),
+              selected: _selectedIndex == 10,
+              onTap: () {
+                _onItemTapped(10);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.library_books),
+              title: const Text('Sổ quỹ'),
+              selected: _selectedIndex == 11,
+              onTap: () {
+                _onItemTapped(11);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Phiếu bàn giao ca'),
+              selected: _selectedIndex == 12,
+              onTap: () {
+                _onItemTapped(12);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.summarize),
+              title: const Text('Báo cáo'),
+              selected: _selectedIndex == 13,
+              onTap: () {
+                _onItemTapped(13);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.attach_money),
+              title: const Text('Bán hàng'),
+              selected: _selectedIndex == 14,
+              onTap: () {
+                _onItemTapped(14);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.table_bar),
+              title: const Text('Phòng/bàn'),
+              selected: _selectedIndex == 15,
+              onTap: () {
+                _onItemTapped(15);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.local_print_shop),
+              title: const Text('Máy in'),
+              selected: _selectedIndex == 16,
+              onTap: () {
+                _onItemTapped(16);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout, color: Colors.red),
+              title:
+                  const Text('Đăng xuất', style: TextStyle(color: Colors.red)),
+              selected: _selectedIndex == 17,
+              onTap: () {
+                _onItemTapped(17);
                 Navigator.pop(context);
               },
             ),
